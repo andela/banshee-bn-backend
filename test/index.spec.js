6e2404cc653ce70bea29f770be42106c71297b68
Desmond Edem <edemintech@gmail.com>
2019-08-15 11:27:36 +0100

    chore(test): setup unit testing
    [Finishes #167891566]

---
+++ test/index.spec.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/index'
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Handle incoming requests on routes', () => {
+  it('should return 200 and success message for the / route', (done) => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('status');
+        expect(res.body.status).to.equal(200);
+        done(err);
+      });
+  });
+});
