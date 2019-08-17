973977f3e34c86283ff726cc0fa9859bc26dc36b
Tijani Oluwafemi <olutimedia@gmail.com>
2019-08-17 13:23:41 +0100

    chore(orm): setup sequelize as ORM
    Sets up the migrations and models structure, includes a dummy user table.
    This setup addresses testing the whole configuration.
    [delivers #167891562]

---
+++ src/database/config/config.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_DEV_NAME, DB_USER_NAME, DB_PASSWORD } = process.env;
+
+const dialect = 'postgres';
+
+module.exports = {
+  development: {
+    username: DB_USER_NAME,
+    password: DB_PASSWORD,
+    database: DB_DEV_NAME,
+    host: '127.0.0.1',
+    port: process.env.DB_PORT,
+    dialect
+  },
+  test: {
+    username: DB_USER_NAME,
+    password: DB_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect
+  },
+  production: {
+    username: process.env.PRODUCTION_USERNAME,
+    password: process.env.PRODUCTION_PASSWORD,
+    database: process.env.PRODUCTION_DATABASE,
+    host: process.env.PRODUCTION_HOST,
+    port: process.env.PRODUCTION_PORT,
+    dialect,
+    ssl: true
+  }
+};
---
+++ src/database/migrations/20190817131405-create-users.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    first_name: {
+      type: Sequelize.STRING
+    },
+    last_name: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    role: {
+      type: Sequelize.STRING
+    },
+    gender: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn('now')
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn('now')
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Users')
+};
