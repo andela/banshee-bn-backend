0e46031b296fb72bb027683fa8b888d24ac70ec8
Your NameSneakymax <maximusekeh@gmail.com>
2019-08-31 22:22:21 +0100

    feature(travel-request): implement create one way travel request feature
    - write tests
    - add validation middleware
    - add create one way travel request route and controller
    
    [Finishes #167891581]

---
+++ test/mockData/mockTrips.js
@@ -0,0 +1,54 @@
+const oneWayTravelRequests = [
+  {
+    from: '3dd3b34a-7554-425e-a688-36afda199614',
+    departureDate: '2000-09-09',
+    reason: 'Just wanna travel',
+    destination:
+    {
+      to: '3dd3b34a-7554-455e-a688-36afda199624',
+      accomodation: '3dd3b34a-7554-425e-a688-36afda199619'
+    }
+  },
+  {
+    from: '3dd3b34a-7554-425e-a688-36afda1996ab',
+    departureDate: '2000-09-09',
+    reason: 'Just wanna travel',
+    destination:
+    {
+      to: '3dd3b34a-7554-455e-a688-36afda199624',
+      accomodation: '3dd3b34a-7554-425e-a688-36afda199619'
+    }
+  },
+  {
+    from: '3dd3b34a-7554-425e-a688-36afda199614',
+    departureDate: '2000-09-09',
+    reason: 'Just wanna travel',
+    destination:
+    {
+      to: '3dd3b34a-7554-455e-a688-36afda199100',
+      accomodation: '3dd3b34a-7554-425e-a688-36afda199619'
+    }
+  },
+  {
+    from: '3dd3b34a-7554-425e-a688-36afda199614',
+    departureDate: '2000-09-09',
+    reason: 'Just wanna travel',
+    destination:
+    {
+      to: '3dd3b34a-7554-425e-a688-36afda199614',
+      accomodation: '3dd3b34a-7554-425e-a688-36afda199619'
+    }
+  },
+  {
+    from: '3dd3b34a-7554-425e-a688-36afda199614',
+    departureDate: '2000-09-09',
+    reason: 'Just wanna travel',
+    destination:
+    {
+      to: '3dd3b34a-7554-455e-a688-36afda199624',
+      accomodation: '3dd3b34a-7554-425e-a688-36afda199100'
+    }
+  },
+];
+
+export default { oneWayTravelRequests };
