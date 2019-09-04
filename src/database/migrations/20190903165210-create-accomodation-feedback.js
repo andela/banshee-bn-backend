953c4a05a0f4349e4925900a5fb2b3661e331dd9
Tijani Oluwafemi <tjhakeemus@gmail.com>
2019-09-04 09:21:33 +0100

    feat(database): Creates accomodation feedback models
     - creates migration
     - creates seeders
     [finishes #167891605]

---
+++ src/database/migrations/20190903165210-create-accomodation-feedback.js
@@ -0,0 +1,61 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('AccomodationFeedbacks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      reviews: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      likeDislike: {
+        allowNull: false,
+        type: Sequelize.ENUM('like', 'dislike')
+      },
+      accomodationId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'Accomodations',
+          key: 'id'
+        }
+      },
+      tripId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'Trips',
+          key: 'id'
+        }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: queryInterface => queryInterface.dropTable('AccomodationFeedbacks')
+};
