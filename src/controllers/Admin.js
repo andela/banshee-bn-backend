8932728d45de94c9d22f008807d33dc7fe9c8c84
Desmond Edem <edemintech@gmail.com>
2019-09-12 03:18:02 +0100

    feat(users): retrieve all users in a company
    [Starts #168435664]

--- src/controllers/Admin.js
+++ src/controllers/Admin.js
@@ -57,6 +57,34 @@ class AdminController {
       return res.status(500).json(new Response(false, 500, error.message));
     }
   }
+
+  /**
+  * Get all users in the company
+  * Route: GET: /user/all
+  * @param {object} req - HTTP Request object
+  * @param {object} res - HTTP Response object
+  * @return {res} res - HTTP Response object
+  * @memberof AdminController
+  */
+  static async getAllUsers(req, res) {
+    const { payload: { companyId } } = req.payload;
+    try {
+      const users = await User.findAll({
+        where: { companyId },
+        attributes: {
+          exclude: ['password']
+        }
+      });
+
+      const response = new Response(true, 200, 'Users retrieved', users);
+      return res.status(response.code).json(response);
+    } catch (error) {
+      const response = new Response(
+        false, 500, 'Server error, Please try again later'
+      );
+      return res.status(response.code).json(response);
+    }
+  }
 }
 
 export default AdminController;
